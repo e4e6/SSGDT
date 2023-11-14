788f727b2445a274735e30f8a328e69550a82e52
e4e6 <hangyeol460@gmail.com>
2023-11-14 15:54:53 +0900

    fix: 가독성을 위해 주석 위치와 문구 수정

--- lib/screens/question_screens/widgets/bundle_question_image_widget.dart
+++ lib/screens/question_screens/widgets/bundle_question_image_widget.dart
@@ -17,14 +17,15 @@ class _BundleQuestionImageWidgetState extends State<BundleQuestionImageWidget> {
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(15),
-      child: Image.asset(
+      child:
+      /* Image.asset에서 Default 이미지인 question_mark.png 는 높이-폭 비율이 같음
+      반면 일반적으로 Image.asset 에 들어갈 이미지들은 높이(height)에 비해 폭(width)이 큼
+      그래서 기준을 폭(width)에 맞추면 (앱이 반응형 ui로 디자인 되지 않았기 때문에) overflow가 발생함
+      그래서 기준을 height, width 중에서 짧은쪽인 height에 맞춰 overflow가 일어나지 않도록 함
+      근본적인 해결 방안은 아니지만 일단 임시방편으로 이렇게 해둠 */
+      Image.asset(
         questionControllerWatch.questionBundleList[questionControllerWatch.questionBundleListIndex].imagePath,
       height:180
-          // 기본 상태로 추가한 이미지 question_mark.png 는 높이폭 비율이 같음
-        // 반면 기존 이미지들은 높이에 비해 폭이 넓음
-        // 그래서 폭에 기준을 맞추면 앱에서 (반응형 ui로 디자인되지 않았기 때문에) overflow가 발생함
-        // 그래서 기준을 높이, 폭 중에서 짧은쪽인 높이에 맞춰 overflow가 일어나지 않도록 함
-        // 근본적인 해결 방안은 아니지만 일단 임시방편으로 이렇게 해둠
       ),
     );
   }
