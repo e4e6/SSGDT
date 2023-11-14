e1e62394868f716fc57035f3c84abf3eb745bf92
e4e6 <hangyeol460@gmail.com>
2023-11-14 09:39:00 +0900

    fix: image overflow 방지

--- lib/screens/question_screens/widgets/bundle_question_image_widget.dart
+++ lib/screens/question_screens/widgets/bundle_question_image_widget.dart
@@ -19,7 +19,12 @@ class _BundleQuestionImageWidgetState extends State<BundleQuestionImageWidget> {
       borderRadius: BorderRadius.circular(15),
       child: Image.asset(
         questionControllerWatch.questionBundleList[questionControllerWatch.questionBundleIndex].imagePath,
-      width:320
+      height:180
+          // 기본 상태로 추가한 이미지 question_mark.png 는 높이폭 비율이 같음
+        // 반면 기존 이미지들은 높이에 비해 폭이 넓음
+        // 그래서 폭에 기준을 맞추면 앱에서 (반응형 ui로 디자인되지 않았기 때문에) overflow가 발생함
+        // 그래서 기준을 높이, 폭 중에서 짧은쪽인 높이에 맞춰 overflow가 일어나지 않도록 함
+        // 근본적인 해결 방안은 아니지만 일단 임시방편으로 이렇게 해둠
       ),
     );
   }
